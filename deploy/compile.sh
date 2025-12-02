0979a7daa149f6781a543b485e21f26aef562db3
Yassine OUAHMANE <yassine@example.com>
2025-12-02 14:06:00 +0000

    Create deployment infrastructure

--- deploy/compile.sh
+++ deploy/compile.sh
@@ -1,3 +1,2 @@
-javac -d bin src/config/*.java src/cluster/*.java src/utils/*.java \
-      src/parser/*.java src/network/worker/*.java \
-      src/network/master/*.java src/scheduler/*.java
+# Create deployment infrastructure
+# File: deploy/compile.sh
